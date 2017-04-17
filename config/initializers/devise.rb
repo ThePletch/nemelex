d8b9907221a6f077a78ee728ce00dd0ef04dbd82
Steve Pletcher <steve@bookbub.com>
2017-04-17 14:31:00 -0400

    full pg switch, try this in prod

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,6 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'eec4756cc78adf4b3cae59c7567ed671ae358caf10ba33cb157ace1e18684da4ccf3cdc76a0aef6a6be617aac87fadfd2d62b4e6c36ee7efd089e2b53715b444'
   config.secret_key = ENV['DEVISE_SECRET_KEY']
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
