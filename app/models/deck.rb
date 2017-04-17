e46125ce10c696b94b38d293f7521f8463642579
Steve Pletcher <steve@bookbub.com>
2017-04-17 16:00:29 -0400

    Remove unneeded validation

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -4,9 +4,6 @@ class Deck < ApplicationRecord
 
   accepts_nested_attributes_for :cards, allow_destroy: true
 
-  validates_numericality_of :initial_hand_size, less_than_or_equal_to: Proc.new(&:number_readied), allow_blank: true,
-    message: "Your initial hand can't be more cards than you have readied."
-
   def number_readied
     cards.readied.count
   end
